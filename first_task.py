7522f0bcb9728796c76536ccdd0525d9752f4241
4attye <167115532+4attye@users.noreply.github.com>
2024-06-30 20:18:38 +0300

    dz

--- first_task.py
+++ first_task.py
@@ -1,22 +1,17 @@
 from datetime import datetime
 
-# Введення дати
-entered_date = input("Enter a date YYYY-MM-DD:")
-
 
 def get_days_from_today(given_date):
-    while True:
-        try:
-            # Перетворення рядка дати у форматі 'YYYY-MM-DD'
-            given_date = datetime.strptime(given_date, '%Y-%m-%d')
-            # Отримання поточної дати
-            current_date = datetime.today()
-            # Розрахунок і повернення різниці між поточною датою та заданою датою
-            return current_date.toordinal() - given_date.toordinal()
-        except ValueError:
-            # В разі помилки виводить "Please enter a valid date YYYY-MM-DD:" та очікує введення коректних данних
-            given_date = input("Please enter a valid date YYYY-MM-DD:")
+    try:
+        # Перетворення рядка дати у форматі 'YYYY-MM-DD'
+        given_date = datetime.strptime(given_date, '%Y-%m-%d')
+        # Отримання поточної дати
+        current_date = datetime.today()
+        # Розрахунок і повернення різниці між поточною датою та заданою датою
+        return current_date.toordinal() - given_date.toordinal()
+    except ValueError:
+        # В разі помилки виводить "Incorrect data entered"
+        print("Incorrect data entered")
 
 
-result = get_days_from_today(entered_date)
-print(result)
+print(get_days_from_today("2021-10-09"))
