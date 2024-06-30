98af26f3994809cad8331fbb9e73e49af2f12473
4attye <167115532+4attye@users.noreply.github.com>
2024-06-30 15:27:58 +0300

    dz

--- first_task.py
+++ first_task.py
@@ -1,10 +1,17 @@
 from datetime import datetime
-enter_date = input("Enter date YYYY-MM-DD:")
+
+enter_date = input("Enter a date YYYY-MM-DD:")
 
 
 def get_days_from_today(date):
-    date = datetime.strptime(date, '%Y-%m-%d')
-    current_date = datetime.today()
-    return current_date.toordinal() - date.toordinal()
+    while True:
+        try:
+            date = datetime.strptime(date, '%Y-%m-%d').date()
+            current_date = datetime.today().date()
+            return current_date.toordinal() - date.toordinal()
+        except ValueError:
+            date = input("Please enter a valid date YYYY-MM-DD :")
 
-print(get_days_from_today(enter_date))
+
+result = get_days_from_today(enter_date)
+print(result)
