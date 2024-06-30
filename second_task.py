3fb888c217a689235e664fd11f58992793521336
4attye <167115532+4attye@users.noreply.github.com>
2024-06-30 17:04:14 +0300

    dz

---
+++ second_task.py
@@ -0,0 +1,17 @@
+import random
+
+
+def get_numbers_ticket(min_num, max_num, quantity):
+    # Перевірка валідності вхідних параметрів
+    if not (1 <= min_num <= max_num <= 1000) or quantity >= max_num or quantity <= 0:
+        return []
+    # Генеруємо множину унікальних випадкових чисел
+    random_numbers = set()
+    while len(random_numbers) < quantity:
+        random_numbers.add(random.randint(min_num, max_num))
+    # Перетворюємо множину в список і сортуємо його
+    return sorted(random_numbers)
+
+
+lottery_numbers = get_numbers_ticket(1, 49, 6)
+print("Ваші лотерейні числа:", lottery_numbers)
