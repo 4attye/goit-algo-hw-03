893bf480d83fc07a63aeccf3fde1947ef767ecca
4attye <167115532+4attye@users.noreply.github.com>
2024-06-30 17:46:21 +0300

    dz

--- second_task.py
+++ second_task.py
@@ -13,5 +13,6 @@ def get_numbers_ticket(min_num, max_num, quantity):
     return sorted(random_numbers)
 
 
+
 lottery_numbers = get_numbers_ticket(1, 49, 6)
 print("Ваші лотерейні числа:", lottery_numbers)
