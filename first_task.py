d5780cd4be570df88857f06e28e03a51a4e1214c
4attye <167115532+4attye@users.noreply.github.com>
2024-06-30 15:52:48 +0300

    dz

--- first_task.py
+++ first_task.py
@@ -8,9 +8,9 @@ def get_days_from_today(given_date):
     while True:
         try:
             # Перетворення рядка дати у форматі 'YYYY-MM-DD'
-            given_date = datetime.strptime(given_date, '%Y-%m-%d').date()
+            given_date = datetime.strptime(given_date, '%Y-%m-%d')
             # Отримання поточної дати
-            current_date = datetime.today().date()
+            current_date = datetime.today()
             # Розрахунок і повернення різниці між поточною датою та заданою датою
             return current_date.toordinal() - given_date.toordinal()
         except ValueError:
